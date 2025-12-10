8eb8b99302d3dd23e7085fed43bd9a59642204d1
Ashraf Morningstar <ashrafmorningstar@gmail.com>
2025-12-10 21:31:33 +0530

    Add complete project structure with config files and components

---
+++ remotion.config.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by AshrafMorningstar
+ * https://github.com/AshrafMorningstar
+ */
+import { Config } from "@remotion/cli/config";
+
+Config.setVideoImageFormat("jpeg");
+Config.setOverwriteOutput(true);
